b79b81361b29d6206f1d1085ccf92fed7f643d9f
massirr <124568542+massirr@users.noreply.github.com>
2025-05-06 20:46:34 +0200

    camps.api

--- routes/camps.js
+++ routes/camps.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with camps');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+/* Get the camps and their info, images included */
+router.get('/', async function(req, res, next) {
+  try {
+    const camps = await prisma.campingSpots.findMany({
+      include: {
+        campingSpot_images: true, // Fetch related images
+      },
+    });
+
+    res.json(camps);
+  } catch (error) {
+    console.error('Error fetching camps:', error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
 });
 
 module.exports = router;
